a4fe6493c4f2c99aca2564154c0cb503515d144c
Your Name <you@example.com>
2020-06-17 20:56:55 +0500

    tableChanges

--- resources/views/reports/pdfViews/awufulReferralBuyerReport.blade.php
+++ resources/views/reports/pdfViews/awufulReferralBuyerReport.blade.php
@@ -9,12 +9,12 @@ Awuf Referral Buyer Report
     margin: 30px auto !important; width: 100%;border-collapse: collapse;">
   <thead class="thead-light">
        <tr>
-          <th scope="col">Sl No</th>
-          <th scope="col">Buyer Name</th>
-          <th scope="col">Buyer  mobile</th>
-          <th scope="col">Total No Refferal</th>
-          <th scope="col">Referral Sale Amount</th>
-          <th scope="col">Earned From Referral</th>
+          <th scope="col" style="padding: 3px 25px !important;">Sl No</th>
+          <th scope="col" style="padding: 3px 25px !important;">Buyer Name</th>
+          <th scope="col" style="padding: 3px 25px !important;">Buyer  mobile</th>
+          <th scope="col" style="padding: 3px 25px !important;">Total No Refferal</th>
+          <th scope="col" style="padding: 3px 25px !important;">Referral Sale Amount</th>
+          <th scope="col" style="padding: 3px 25px !important;">Earned From Referral</th>
           <th>Referral Details</th>
       </tr>
     </thead>
@@ -23,17 +23,17 @@ Awuf Referral Buyer Report
 
         @forelse($buyers as $buyer)
         <tr>
-           <td ><a >{{$loop->iteration}}</a></td>
+           <td  style="padding: 3px 25px !important;"><a >{{$loop->iteration}}</a></td>
            <td  ><a >{{$buyer->full_name}}</a></td>
-           <td>{{$buyer->mobile}}</td>
-           <td>{{$buyer->phone}}</td>
-           <td>{{$buyer->created_at}}</td>
+           <td style="padding: 3px 25px !important;">{{$buyer->mobile}}</td>
+           <td style="padding: 3px 25px !important;">{{$buyer->phone}}</td>
+           <td style="padding: 3px 25px !important;">{{$buyer->created_at}}</td>
           
            <td>{{$buyer->referrals[0]->earnedFromReferral}}</td>
         </tr>
         @empty
         <tr>
-           <td>Nothing Found!</td>
+           <td style="padding: 3px 25px !important;">Nothing Found!</td>
         </tr>
         @endforelse
      </tbody>
